81f3fb5ee44a7f6a24b3a4792cff7ceae223c71a
Antje Heiermann <antje.heiermann@web.de>
2020-11-13 12:55:43 +0100

    Routes, Controllers, Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
 
+resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :review, only: [:create]
+  end
+    resources :doses, only: [:create]
 end
